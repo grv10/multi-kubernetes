ea3280f77fc702b68004e63d3848f9722cb13458
grv2730 <gauravdwivedi10@gmail.com>
2019-03-23 04:47:40 -0400

    change

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -39,7 +39,7 @@ class Fib extends Component {
   //   console.log(this.state.seenIndexes);
   //   return this.state.seenIndexes.map(({ number }) => number).join(', ');
   // }
-
+  
   renderSeenIndexes = () => {
     let indexes = '';
 
